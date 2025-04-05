d9e3d4f3e48c1523e817f7f36d1e941d807db43e
Rakshitha Kolluru <rakshithakolluru7@gmail.com>
2025-04-05 23:38:12 +0530

    Data Ingestion and Validation Done

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,22 +1,18 @@
 import sys
-from networksecurity.logging import logger
+from networksecurity.logging.logger import logger
 
 class NetworkSecurityException(Exception):
-    def __init__(self,error_message,error_details:sys):
+    def __init__(self, error_message, error_details: sys):
         self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
+        _, _, exc_tb = error_details.exc_info()
         
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename 
-    
+        self.lineno = exc_tb.tb_lineno if exc_tb else "unknown"
+        self.file_name = exc_tb.tb_frame.f_code.co_filename if exc_tb else "unknown"
+
+        logger.error(self.__str__())
+
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
-        
-if __name__=='__main__':
-    try:
-        logger.logging.info("Enter the try block")
-        a=1/0
-        print("This will not be printed",a)
-    except Exception as e:
-           raise NetworkSecurityException(e,sys)
+        return (
+            f"Error occured in python script name [{self.file_name}] "
+            f"line number [{self.lineno}] error message [{self.error_message}]"
+        )
