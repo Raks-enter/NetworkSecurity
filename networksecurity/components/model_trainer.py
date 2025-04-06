aa626a575963a475a983dd02f5b06decdf436f4b
Rakshitha Kolluru <rakshithakolluru7@gmail.com>
2025-04-06 21:22:05 +0530

    pipeline Implemented

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -27,8 +27,7 @@ import mlflow
 from urllib.parse import urlparse
 
 import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
-
+dagshub.init(repo_owner='Raks-enter', repo_name='NetworkSecurity', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -121,6 +120,7 @@ class ModelTrainer:
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
         #model pusher
+        save_object("final_model/model.pkl",best_model)
         
 
         ## Model Trainer Artifact
